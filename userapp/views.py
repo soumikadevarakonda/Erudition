0035d1dda4fca040fe00294ee44d6f0ecc893280
soumikadevarakonda <soumikadevarakonda@gmail.com>
2024-12-07 23:17:15 +0530

    User App connecting to Admin Catalog

--- userapp/views.py
+++ userapp/views.py
@@ -2,9 +2,16 @@
 from django.shortcuts import render
 from projectapp.models import Book
 
+
 def userhomepage(request):
-    # Fetch all books from the catalog
-    books = Book.objects.all()
+    # Get the search query from the GET request, if present
+    query = request.GET.get('q', '')  # Default to an empty string if no query
+
+    # Filter books based on the query if it's present, otherwise fetch all books
+    if query:
+        books = Book.objects.filter(title__icontains=query)  # Case-insensitive search in the title
+    else:
+        books = Book.objects.all()  # Fetch all books if no search query is given
 
     # Pass the books to the template
     return render(request, 'userapp/UserHomePage.html', {'books': books})
